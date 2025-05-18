6a6f3efb9f047087cd549dbe1cd126ccc63f7e45
dm <druavhm@gmail.com>
2025-05-19 02:47:13 +0530

    style ui, minor bug fixes & optimisations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,77 +1,113 @@
-import { useState } from "react"
 import useDownloader from "./store/store"
+import { FiLoader } from "react-icons/fi";
+
 
 function App() {
-  const [url, setUrl] = useState<string>("")
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const { format, setFormat, setVideoInfo } = useDownloader()
+
+  const { url, videoInfo, format, isLoading, setUrl, setVideoInfo, setFormat, setIsLoading } = useDownloader()
 
   const handleDownload = async () => {
+    if (!url) return alert("Please enter a valid YouTube URL")
     setIsLoading(true)
+
     try {
-      const res = await fetch('http://localhost:5000/api/download', {
+      const res = await fetch('http://localhost:5000/api/info', {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ url, format })
+        body: JSON.stringify({ url })
       })
 
       if (!res.ok) {
-        throw new Error("Failed to download")
+        alert("Failed to fetch video information!")
+        return
       }
-      // blob is binary large object (immutable, file like data type)
-      const blob = await res.blob()
 
-      const downloadUrl = window.URL.createObjectURL(blob)
+      const data = await res.json()
+      setVideoInfo(data)
 
-      const link = document.createElement('a')
-      link.href = downloadUrl
-      link.download = `download.${format}`
-      link.click()
-
-      setVideoInfo({ url: downloadUrl })
-
-    } catch (error) {
-      console.error("Download failed...", error)
+    } catch (err) {
+      alert("Error fetching video info")
+      console.error("Error while fetching the video information: ", err)
     } finally {
       setIsLoading(false)
     }
   }
 
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUrl(e.target.value)
+    setVideoInfo(null)
+  }
 
   return (
-    <div className="min-h-screen flex justify-center bg-gray-900 font-mono text-white">
-      <div className="w-3xl flex flex-col gap-10">
-        <h1 className="py-8 text-xl text-center">youtubeDL</h1>
+    <div className="min-h-screen px-3 flex items-center justify-center bg-gray-900 font-mono text-white">
+      <div className="w-full max-w-lg flex flex-col gap-6">
+        <h1 className="py-4 text-xl text-center font-bold">youtubeDL</h1>
         <div className="flex gap-3">
           <input
             type="text"
             value={url}
-            onChange={(e) => setUrl(e.target.value)}
-            placeholder="Paste Youtube link..."
-            className="p-2 flex-1 outline-none focus:border-b"
+            onChange={handleUrlChange}
+            placeholder="Enter YouTube URL..."
+            className="p-3 flex-1 outline-none bg-gray-800 focus:border-b border-gray-100 rounded-sm"
           />
           <select
             value={format}
             onChange={(e) => setFormat(e.target.value)}
-            className="px-4 text-white outline-none border-l cursor-pointer"
+            className="p-3 text-white outline-none bg-gray-800 border-l border-gray-100 cursor-pointer rounded-sm"
           >
-            <option value="mp4" className="text-black">Video</option>
-            <option value="mp3" className="text-black">Audio</option>
+            <option value="mp4" className="text-white">Video</option>
+            <option value="mp3" className="text-white">Audio</option>
           </select>
         </div>
 
         <button
           onClick={handleDownload}
-          className={`p-2 bg-blue-700 hover:bg-blue-800 rounded w-full cursor-pointer 
-            ${isLoading ? "opacity-50 cursor-not-allowed" : ""}`}
-          disabled={isLoading}
+          disabled={isLoading || (!!videoInfo && !!url)}
+          className={`p-3 bg-blue-700 hover:bg-blue-800 rounded-sm w-full transition
+            ${isLoading || (!!videoInfo && !!url) ? "opacity-50 cursor-not-allowed" : "cursor-pointer "}
+            `}
         >
-          {isLoading ? "Downloading..." : "Download"}
+          {isLoading ? "Fetching Video Info..." : videoInfo ? "Link Loaded - Click to Download" : "Get Download Link"}
         </button>
 
         {isLoading && (
-          <div className="text-center mt-4 animate-pulse">Downloading... Please wait.</div>
+          <div className="py-5 flex flex-col gap-4 items-center justify-center text-gray-200 ">
+            <FiLoader className="size-8 animate-spin" />
+            <p className="animate-pulse">
+              Loading... Please wait.
+            </p>
+          </div>
         )}
+
+        {(!isLoading && videoInfo) ? (
+          <div className="p-4 bg-gray-800 rounded-sm">
+            <img src={videoInfo.thumbnail_url} alt="Video Thumbnail" className="w-full mb-4 rounded-sm" />
+            <p className="text-lg text-pretty font-semibold">{videoInfo.title}</p>
+            <a href={`http://localhost:5000/api/download?url=${encodeURIComponent(url)}&format=${format}`} download={`download.${format === 'mp3' ? 'mp3' : 'mp4'}`} >
+              <button
+                className="mt-4 p-3 w-full text-center cursor-pointer bg-green-600 hover:bg-green-700 rounded-sm"> Download {format === 'mp3' ? 'Audio' : 'Video'}
+              </button>
+            </a>
+          </div>
+        )
+          :
+          (
+            <div className="min-h-40 flex flex-col justify-center">
+
+              <div className=" w-full min-h-20 p-3 flex flex-col gap-5 rounded bg-gray-800 justify-center font-mono text-sm text-pretty relative">
+                <span className="absolute size-[10px] left-[-3px] top-[-2px] bg-gray-200 rounded-full animate-pulse" />
+                <p>Just paste the link of your favorite
+                  {" "}<span className="text-gray-100 border-b-[1px] border-gray-400">Youtube video</span>,
+                  {" "}<span className="text-gray-100 border-b-[1px] border-gray-400">Youtube short</span>, or
+                  {" "}<span className="text-gray-100 border-b-[1px] border-gray-400">Youtube music</span>{" "}
+                  and download it's high quality audio or video for FREE!
+                </p>
+                <p>Due to strict YouTube guidelines and security changes, sometimes the server would be down and cause inconvenience to you.</p>
+              </div>
+            </div>
+          )
+        }
+
       </div>
     </div>
   )
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -2,21 +2,30 @@ import { create } from "zustand"
 
 interface VideoInfo {
     title?: string
-    url: string
+    thumbnail_url?: string
+    video_url?: string
 }
 
 interface DownloadState {
+    url: string
     videoInfo: VideoInfo | null;
     format: string
-    setVideoInfo: (info: VideoInfo) => void
+    isLoading: boolean
+    setUrl: (link: string) => void
+    setVideoInfo: (info: VideoInfo | null) => void
     setFormat: (format: string) => void
+    setIsLoading: (bool: boolean) => void
 }
 
 const useDownloader = create<DownloadState>()((set) => ({
+    url: '',
     videoInfo: null,
-    setVideoInfo: (info) => set({ videoInfo: info }),
     format: "mp4",
-    setFormat: (format) => set({ format })
+    isLoading: false,
+    setUrl: (link) => set({ url: link }),
+    setVideoInfo: (info) => set({ videoInfo: info }),
+    setFormat: (format) => set({ format }),
+    setIsLoading: (bool) => set({ isLoading: bool })
 }))
 
 export default useDownloader

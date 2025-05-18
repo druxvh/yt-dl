2d4297db1a7673d712b3cb681156994b068a0685
dm <druavhm@gmail.com>
2025-05-18 22:41:20 +0530

    feat gets vid details from yt & optimised download video/audio

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express"
 import cors from "cors"
-import ytdl from "@distube/ytdl-core";
+import ytdl, { downloadOptions } from "@distube/ytdl-core";
+import { promisify } from "util";
+import { pipeline } from "stream";
 
 const app = express()
+const asyncPipeline = promisify(pipeline)
 
 const corsOptions = {
     origin: ['http://localhost:5173'], // Allow requests only from these origins
@@ -15,35 +18,57 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.use(express.json())
 
-app.use("/api/download", async (req: Request, res: Response) => {
-    const { url, format } = req.body
+// download video/audio
+app.get("/api/download", async (req: Request, res: Response) => {
+    const { url, format } = req.query
 
+    // validates youtube url
+    if (!ytdl.validateURL(String(url))) {
+        res.status(400).json({ error: "Invalid URL" })
+        return
+    }
     try {
-        // validates youtube url
-        if (!ytdl.validateURL(url)) {
-            res.status(400).json({ error: "Invalid URL" })
-            return
+        // ytdl options
+        const options: downloadOptions = {
+            filter: format === 'mp3' ? 'audioonly' : 'audioandvideo',
+            quality: format === 'mp3' ? 'highestaudio' : 'highestvideo'
         }
-        // gets the video
-        const info = await ytdl.getInfo(url)
-        const title = info.videoDetails.title
-
+        const videoInfo = await ytdl.getInfo(String(url))
+        const formatInfo = ytdl.chooseFormat(videoInfo.formats, options)
         // sets header to let browser know bout the downloadable attachment file
         res.setHeader(
             "Content-Disposition",
-            `attachment; filename="${title}.${format}"`
+            `attachment; filename="download.${format === 'mp3' ? 'mp3' : 'mp4'}"`
         )
+        // header to let browser know bout the content type of the data
+        res.setHeader(
+            "Content-Type", format === 'mp3' ? 'audio/mpeg' : 'video/mp4'
+        )
+        // set content-length header only if available
+        if (formatInfo.contentLength) {
+            res.setHeader("Content-Length", formatInfo?.contentLength);
+        }
 
-        const stream = ytdl(url, {
-            filter: format === 'mp3' ? 'audioonly' : 'audioandvideo',
-            quality: format === 'mp3' ? 'highestaudio' : 'highestvideo'
+        const stream = ytdl(String(url), options)
+        await asyncPipeline(stream, res)
+
+    } catch (err) {
+        res.status(500).json({ error: "Error processing video", err })
+    }
+})
+
+// get video details from the url
+app.post('/api/info', async (req: Request, res: Response) => {
+    const { url } = req.body
+    try {
+        const videoInfo = await ytdl.getInfo(url)
+        res.status(200).json({
+            title: videoInfo.videoDetails.title,
+            thumbnail_url: videoInfo.videoDetails.thumbnails[0].url,
+            video_url: videoInfo.videoDetails.video_url
         })
-
-        stream.pipe(res)
-
-    } catch (error) {
-        console.error("Error processing video:", error);
-        res.status(500).json({ error: "Error processing video" })
+    } catch (err) {
+        res.status(500).json({ error: "Error retrieving video info", err })
     }
 })
 
